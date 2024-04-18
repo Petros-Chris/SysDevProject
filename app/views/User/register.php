bfd6ef1f0dec2a6f24d4064572fb31f7c68149e1
chrischiken <2271356@edu.vaniercollege.qc.ca>
2024-04-18 11:18:11 -0400

    made daatabase supposly work with register

--- app/views/User/register.php
+++ app/views/User/register.php
@@ -8,14 +8,14 @@
 	<div class='container'>
 		<form method='post' action=''>
 			<div class="form-group">
-				<label>Username:<input type="text" class="form-control" name="username" placeholder="Jon" /></label>
+				<label>Username:<input type="text" class="form-control" name="username"/></label>
 			</div>
 			<div class="form-group">
-				<label>Password:<input type="password" class="form-control" name="password" placeholder="password" /></label>
+				<label>Password:<input type="password" class="form-control" name="password"/></label>
 			</div>
 
 			<div class="form-group">
-				<input type="submit" name="action" value="Register" /> 
+				<input type="submit" name="action" value="Register"/> 
 				<a href='/User/login'>I have an account, bring me to the login page</a>
 			</div>
 		</form>
