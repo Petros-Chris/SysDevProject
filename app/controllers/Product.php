7824d462b49ff9c183f149b6cc7a3877b678eca8
chrischiken <2271356@edu.vaniercollege.qc.ca>
2024-05-12 23:28:53 -0400

    helped emmualel with image routing, removed things that don't make sense to be there
    also made a folder for all the images

--- app/controllers/Product.php
+++ app/controllers/Product.php
@@ -4,137 +4,81 @@ namespace app\controllers;
 
 use stdClass;
 
-class Product extends \app\core\Controller {
+class Product extends \app\core\Controller
+{
 
-    function listings() {
+    function listings()
+    {
         $product = new \app\models\Product();
         $productCont = new \app\controllers\Product();
 
-        if(isset($_GET['filter'])) {
-            $productCont -> listingFilter($product, $_GET['type'], $_GET['filter']);
-        } else { 
-		$products = $product->getAll();
+        if (isset($_GET['filter'])) {
+            $productCont->listingFilter($product, $_GET['type'], $_GET['filter']);
+        } else {
+            $products = $product->getAll();
 
-        //This works to display the search bar first but maybe we can find a better way?
-        //Maybe you could make it add each product to a div instead of just at the bottom of the page
-        echo"<form method='POST' action='/Product/search'>
-                <input name='search_box' placeholder='eg: Blue'>
-                <input type='submit' name='action' value='color'>
-                <input type='submit' name='action' value='content'>
-            </form>";
-
-        foreach ($products as $product) {
-            $pro_id = $product->product_id;
-			$pro_brand = $product->brand;
-			$pro_model = $product->model;
-			$pro_color = $product->color;
-            $pro_price = $product->cost_price;
-            $pro_shape = $product->shape;
-            $pro_size = $product->size;
-            $pro_optial_sun = $product->optical_sun;
-            $pro_description = $product->description;
-            echo "<a href='../Product/index?id=$pro_id'> <div class='product-container'>
-                    <div class='product-image'>
-                    <img src='/../app/questionMark.png' alt='$pro_description'>
-                        </div>
-                            <div class='product-details'></a>
-                            <span class='heart-icon' onclick='toggleHeart(this, $pro_id)'>&#x2661;</span>
-                            <div class='product-brand'>$pro_brand</div>
-                            <div class='product-price'>$$pro_price</div>
-                        </div>
-                    </div>";
+            include 'app/views/Product/listing.php';
+            include 'app/views/footer.php';
         }
     }
-        // $this->view('Product/listing');
-    }
 
-    public function listingFilter($product, $type, $filter) {
+    public function listingFilter($product, $type, $filter)
+    {
         $products = $product->getFilter($type, $filter);
         include 'app/views/Product/listing.php';
         include 'app/views/footer.php';
-        // $this->view('Product/listing', $products);
     }
 
-    // function listingFilter($product, $type, $filter) {
-	// 	$products = $product->getFilter($type, $filter);
 
-    //     //This works to display the search bar first but maybe we can find a better way?
-    //     //Maybe you could make it add each product to a div instead of just at the bottom of the page
-    //     echo"<form method='POST' action='/Product/search'>
-    //             <input name='search_box' placeholder='eg: Blue'>
-    //             <input type='submit' name='action' value='color'>
-    //             <input type='submit' name='action' value='content'>
-    //         </form>";
-
-    //     foreach ($products as $product) {
-    //         $pro_id = $product->product_id;
-	// 		$pro_brand = $product->brand;
-	// 		$pro_model = $product->model;
-	// 		$pro_color = $product->color;
-    //         $pro_price = $product->cost_price;
-    //         $pro_shape = $product->shape;
-    //         $pro_size = $product->size;
-    //         $pro_optial_sun = $product->optical_sun;
-    //         $pro_description = $product->description;
-    //         echo "<a href='../Product/index?id=$pro_id'> <div class='product-container'>
-    //                 <div class='product-image'>
-    //                 <img src='/../app/questionMark.png' alt='$pro_description'>
-    //                     </div>
-    //                         <div class='product-details'></a>
-    //                         <span class='heart-icon' onclick='toggleHeart(this, $pro_id)'>&#x2661;</span>
-    //                         <div class='product-brand'>$pro_brand</div>
-    //                         <div class='product-price'>$$pro_price</div>
-    //                     </div>
-    //                 </div>";
-    //     }
-    // }
-        
-
-    function description() {
+    function description()
+    {
         $product = new \app\models\Product();
         $item = $product->getId($_GET['id']);
-        
+
         $_SESSION['product_id'] = $_GET['id'];
 
-            
-           
-            
-            $this->view('Product/index', $item);
-            $this->displayCart();
+
+
+
+        $this->view('Product/index', $item);
+        $this->displayCart();
         $review = new \app\controllers\Review();
         $review->displayReview();
-    }    
+    }
 
-    function displayCart(){
-       
-        $this->viewCart();  
+    function displayCart()
+    {
+
+        $this->viewCart();
 
     }
 
-    public function addToCart() {
+    public function addToCart()
+    {
 
-        if(isset($_POST['id'])) {
+        if (isset($_POST['id'])) {
             $product = new \app\models\Product();
             $item = $product->getId($_POST['id']);
 
             if (isset($_SESSION['cart'])) {
                 $cart = $_SESSION['cart'];
                 $length = count($cart);
-                $_SESSION['cart'][$length] = $item; 
+                $_SESSION['cart'][$length] = $item;
             } else {
                 $_SESSION['cart'][0] = $item;
             }
-            
+
         }
     }
 
-    public function viewCart() {
+    public function viewCart()
+    {
         if (isset($_SESSION['cart'])) {
-            
+
             $cart = ($_SESSION['cart']);
             $price = 0;
 
-            foreach($cart as $product) {
+            foreach ($cart as $product) {
                 $pro_id = $product->product_id;
                 $pro_brand = $product->brand;
                 $pro_model = $product->model;
@@ -146,15 +90,15 @@ class Product extends \app\core\Controller {
                 $pro_description = $product->description;
                 $price += $pro_price;
 
-                echo("HI");
+                echo ("HI");
                 echo "<script> 
                         document.getElementById('popup').innerHTML += 
                         '<a href=\"/Product/index?id=$pro_id\">$pro_brand $pro_shape $pro_price</a>' +
                         '<span onclick=\"removeProductFromCart($pro_id)\">&#128465;</span><br>';
-                    </script>";  
+                    </script>";
             }
-                sleep(2);
-                echo "<script> 
+            sleep(2);
+            echo "<script> 
                         document.getElementById('popup').innerHTML += 
                         'Total: $price <br> <input type=button value=proceed>';
 
@@ -167,11 +111,12 @@ class Product extends \app\core\Controller {
                     </script>";
         }
     }
-    
-    public function removeFromCart() {
+
+    public function removeFromCart()
+    {
         if (isset($_SESSION['cart'])) {
 
-            if(isset($_POST['id'])) {
+            if (isset($_POST['id'])) {
                 $cart = $_SESSION['cart'];
 
                 $index = array_search($_POST['id'], array_column($cart, 'product_id'));
@@ -184,31 +129,33 @@ class Product extends \app\core\Controller {
         }
     }
 
-    public function search() {
+    public function search()
+    {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $searchTerm = $_POST['search_box'];
-			$product = new \app\models\Product();   
+            $product = new \app\models\Product();
 
-                $this->view('/Product/listing');
+            $this->view('/Product/listing');
 
             if ($_POST['action'] == 'color') {
-               $products = $product->getColor($searchTerm);
-			   
-				foreach ($products as $producta) {
+                $products = $product->getColor($searchTerm);
+
+                foreach ($products as $producta) {
                     $pro_id = $producta->product_id;
-					$pro_brand = $producta->brand;
-					echo "<a href='../Product/index?brand=$pro_brand&id=$pro_id'>$pro_brand</a><br>";
-				}
-            } 
+                    $pro_brand = $producta->brand;
+                    echo "<a href='../Product/index?brand=$pro_brand&id=$pro_id'>$pro_brand</a><br>";
+                }
+            }
         }
     }
 
-    public function viewCartCheckout() {
+    public function viewCartCheckout()
+    {
         if (isset($_SESSION['cart'])) {
             $cart = ($_SESSION['cart']);
             $price = 0;
 
-            foreach($cart as $product) {
+            foreach ($cart as $product) {
                 $pro_id = $product->product_id;
                 $pro_brand = $product->brand;
                 $pro_model = $product->model;
@@ -223,10 +170,10 @@ class Product extends \app\core\Controller {
                 echo "<p>
                         <a href=\"/Product/index?id=$pro_id\">$pro_brand $pro_shape $pro_price</a>
                         <span onclick=\"removeProductFromCart($pro_id)\">&#128465;</span><br>
-                    </p>" ;
+                    </p>";
             }
-                
-                echo "<p>
+
+            echo "<p>
                         Total: $price <br>
                     </p>";
         }
