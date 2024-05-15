ab1438ba2a1c919219821c5a2146cdf1f3000f97
emmanuelAighbokhan <emmanuelcarter512@gmail.com>
2024-05-15 18:41:04 -0400

    fix logo

--- app/views/nav.php
+++ app/views/nav.php
@@ -10,7 +10,7 @@
 </head>
 <nav class="menu">
     <ol>
-    <a href="/Customer/home"> <img src="/app/resources/logo/LOGO/mesyeuxtesyeuxLOGO.png" alt="Image Logo" height="20%" ; width="20%" ;></a>
+    <a href="/home"> <img src="/app/resources/logo/LOGO/mesyeuxtesyeuxLOGO.png" alt="Image Logo" height="20%" ; width="20%" ;></a>
         <?php if (!isset($_SESSION['employee_id'])): ?>
             <li class="menu-item"><a href="\Product\listing">Shop</a>
                 <ol class="sub-menu">
