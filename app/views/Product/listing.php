39adda53667dd05a5f54bedc0252038d19bb4f40
emmanuelAighbokhan <emmanuelcarter512@gmail.com>
2024-05-15 19:26:08 -0400

    moved css to scss

--- app/views/Product/listing.php
+++ app/views/Product/listing.php
@@ -2,8 +2,8 @@
 <html>
 
 <head>
-    <title><?= $name ?> view</title>
-    <link rel="stylesheet" type="text/css" href="/app/style.scss">
+	<title><?= $name ?> view</title>
+	<link rel="stylesheet" type="text/css" href="/app/css/style.scss">
 </head>
 <body>
 
