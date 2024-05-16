af5229fdefc9b44f9c851f21f8632bafdd87ee0b
emmanuelAighbokhan <emmanuelcarter512@gmail.com>
2024-05-15 21:17:21 -0400

    nn

--- app/views/contact.php
+++ app/views/contact.php
@@ -16,21 +16,21 @@
     <div class="email-box">
         <img src="/app/resources/email.png" alt="Email Icon" class = "imgEmail">
         <h3><?= __('Send an Email') ?></h3>
-        <p><?= __('Want to contact us by email to ask any questions?') ?>Want to contact us by email to ask any questions?</p>
-        <p><?= __('Want to get in touch?') ?>Email to company</p>
-        <a href="mailto:MesYeuxTesYeux@gmail.com"><?= __('Want to get in touch?') ?>MesYeuxTesYeux@gmail.com</a>
-        <p><?= __('Want to get in touch?') ?>Talk to a representative</p>
-        <a href="mailto:PersonCompany@gmail.com"><?= __('Want to get in touch?') ?>PersonCompany@gmail.com</a>
+        <p><?= __('Want to contact us by email to ask any questions?') ?></p>
+        <p><?= __('Email to company') ?></p>
+        <a href="mailto:MesYeuxTesYeux@gmail.com"><?= __('MesYeuxTesYeux@gmail.com') ?></a>
+        <p><?= __('Talk to a representative') ?></p>
+        <a href="mailto:PersonCompany@gmail.com"><?= __('PersonCompany@gmail.com') ?></a>
     </div>
 
     <div class="phone-box">
     <img src="/app/resources/telephone.png" alt="phone Icon" class = "imgPhone">
-        <h3>Give us a call</h3>
-        <p>Would you like to talk to one of our agents?</p>
-        <p>Call our in-store agent</p>
-        <a href="tel:514-341-202">514-341-202</a>
-        <p>Prefer to book an appointment with one of our doctors?</p>
-        <a href="tel:450-xxx-xxx">450-xxx-xxx</a>
+        <h3><?= __('Give us a call') ?></h3>
+        <p><?= __('Would you like to talk to one of our agents?') ?></p>
+        <p><?= __('Call our in-store agent') ?></p>
+        <a href="tel:514-341-202"><?= __('514-341-2020') ?></a>
+        <p><?= __('Prefer to book an appointment with one of our doctors?') ?></p>
+        <a href="tel:450-xxx-xxx"><?= __('450-xxx-xxx') ?></a>
     </div>
 </div>
 
--- app/views/nav.php
+++ app/views/nav.php
@@ -15,7 +15,7 @@
 </head>
 <nav class="menu">
     <ol>
-    <a href="/home"> <img src="/app/resources/logo/LOGO/mesyeuxtesyeuxLOGO.png" class="logo" alt="Image Logo" height="55px" ; width="160px" ;></a>
+    <a href="/home"> <img src="/app/resources/logo/mesyeuxtesyeuxLOGO.png" class="logo" alt="Image Logo" height="55px" ; width="160px" ;></a>
         <?php if (!isset($_SESSION['employee_id'])): ?>
             <li class="menu-item"><a href="\Product\listing">Shop</a>
                 <ol class="sub-menu">
