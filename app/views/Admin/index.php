7843d4fb87cf3004e0dbb1f2bddc3d04632dcaa6
emmanuelAighbokhan <emmanuelcarter512@gmail.com>
2024-05-27 15:00:51 -0400

    minor changes

--- app/views/Admin/index.php
+++ app/views/Admin/index.php
@@ -1,14 +1,16 @@
+<!DOCTYPE html>
 <html>
-    <head>
-	    <title><?= $name ?> view</title>
-	</head>
-    
+<head>
+	<title><?= $name ?> view</title>
+	<link rel="stylesheet" type="text/css" href="/app/css/style.scss">
+</head>
     <body>
-    <button class="Choice-button" onclick="location.href='/Admin/create'"><?=__('Add A New Product')?></button>
-        <button class="Choice-button" onclick="location.href='/Admin/productListing'"><?=__('Modify A Product')?></button>
-        <button class="Choice-button" onclick="location.href='/Admin/customerList'"><?=__('Disable/Enable User Account')?></button>
-        <button class="Choice-button" onclick="location.href='/Ticket/ongoing'"><?=__('View All Ongoing Tickets')?></button>
-        <button class="Choice-button" onclick="location.href='/Employee/creation'"><?=__('Create A New Employee')?></button>
-        <button class="Choice-button" onclick="location.href='/Admin/orders'"><?=__('View All Orders')?></button>
+        <h1 id ="admin"> Admin Control </h1>
+    <button class="Choice-buttonadmin" onclick="location.href='/Admin/create'"><?=__('Add A New Product')?></button>
+        <button class="Choice-buttonadmin" onclick="location.href='/Admin/productListing'"><?=__('Modify A Product')?></button>
+        <button class="Choice-buttonadmin" onclick="location.href='/Admin/customerList'"><?=__('Disable/Enable User Account')?></button>
+        <button class="Choice-buttonadmin" onclick="location.href='/Ticket/ongoing'"><?=__('View All Ongoing Tickets')?></button>
+        <button class="Choice-buttonadmin" onclick="location.href='/Employee/creation'"><?=__('Create A New Employee')?></button>
+        <button class="Choice-buttonadmin" onclick="location.href='/Admin/orders'"><?=__('View All Orders')?></button>
     </body>
 </html>
