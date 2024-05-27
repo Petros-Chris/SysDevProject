c0b0523dfdd731b6c6016a8b7e115e5274234bdc
emmanuelAighbokhan <emmanuelcarter512@gmail.com>
2024-05-26 21:25:50 -0400

    fixed index, added spacing

--- app/views/Product/index.php
+++ app/views/Product/index.php
@@ -9,36 +9,37 @@
 
 <body>
     <img src="/app/resources/images/product_<?= $item->product_id ?>.png" alt="<?= $item->description ?>"
-        style="width: 340px; height: 150px;" class="glasses">
-    <div class="containers">
+        class="glasses">
+        <div class="containers">
+    <dl class="big-words">
+        <dt><?= __('Brand:') ?></dt>
+        <dd><?= $item->brand ?></dd>
+        <dt><?= __('Shape:') ?></dt>
+        <dd><?= $item->shape ?></dd>
+        <dt><?= __('Description:') ?></dt>
+        <dd><?= $item->description ?></dd>
+        <script>document.write(generateStarRating(<?= $product->rating ?>))</script>
+        (<?= $product->how_many_reviews ?>)
+        <dt><?= __('Color:') ?></dt>
+        <dd><?= $item->color ?></dd>
+        <dt><?= __('Cost Price:') ?></dt>
+        <dd><?= $item->sell_price ?></dd>
+        <dt><?= __('Size:') ?></dt>
+        <dd><?= $item->size ?></dd>
+        <dt><?= __('Left In Stock:') ?></dt>
+        <dd><?= $item->quantity ?></dd>
+    </dl>
 
-        <dl>
-            <dt><?= __('Brand:') ?></dt>
-            <dd><?= $item->brand ?></dd>
-            <dt><?= __('Description:') ?></dt>
-            <dd><?= $item->description ?></dd>
-            <dt><?= __('Size:') ?></dt>
-            <dd><?= $item->size ?></dd>
-            <dt><?= __('Shape:') ?></dt>
-            <dd><?= $item->shape ?></dd>
-            <dt><?= __('Color:') ?></dt>
-            <dd><?= $item->color ?></dd>
-            <dt><?= __('Cost Price:') ?></dt>
-            <dd><?= $item->sell_price ?></dd>
-            <dt><?= __('Left In Stock:') ?></dt>
-            <dd><?= $item->quantity ?></dd>
-            <script>document.write(generateStarRating(<?= $product->rating ?>))</script>
-            (<?= $product->how_many_reviews ?>)
-        </dl>
+
+
 
         <select id="quantitySeleted">
             <?php
             for ($num = 1; $num <= $item->quantity; $num++): ?>
                 <option value="<?= $num ?>"><?= $num ?></option>
-
             <?php endfor; ?>
         </select>
-        <input id="cartBtn" type='button' value='Add To Cart'
+        <input id="cartBtn" type='button' value='Add To Cart' style='background-color: blue; color: white;'
             onclick="addProduct(<?= $item->product_id ?>, document.getElementById('quantitySeleted').value)">
     </div>
 </body>
